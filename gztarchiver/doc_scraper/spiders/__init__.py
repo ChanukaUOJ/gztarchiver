0ee551491b5fbded949d8821cdfdae60907b95a9
ChanukaUOJ <chamithroxmpc@gmail.com>
2026-09-11 16:27:35 +0530

    removed the legacy stale files

--- gztarchiver/doc_scraper/spiders/__init__.py
+++
@@ -1,9 +0,0 @@
-from gztarchiver.doc_scraper.v1.spiders.years_spider import YearsSpider
-from gztarchiver.doc_scraper.v1.spiders.doc_metadata_spider import DocMetadataSpider
-from gztarchiver.doc_scraper.common.spiders.doc_download_spider import PDFDownloaderSpider
-
-__all__ = [
-    "YearsSpider",
-    "DocMetadataSpider",
-    "PDFDownloaderSpider"
-]
